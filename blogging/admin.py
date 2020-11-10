86cf173172300f3c8a53a3111e979af3825a18c3
Steven Blanton <stvblanton@outlook.com>
2020-11-09 21:08:47 -0800

    adding new many to many features

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class PostCategory(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        PostCategory,
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts', )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
